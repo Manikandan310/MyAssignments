39a7caf6de1bc7c364c22dd4873123daaf8a8a5f
Manikandan310 <89017595+Manikandan310@users.noreply.github.com>
2026-06-14 19:55:19 +0530

    Add files via upload

---
+++ Week1_Day2/Assignment1.js
@@ -0,0 +1,28 @@
+let firstname="Mani"
+console.log(firstname); 
+console.log(typeof firstname);
+
+
+let comapnyname="Kor"
+console.log(comapnyname);
+console.log(typeof comapnyname);
+
+ 
+let mobilenumber=9344272841  
+console.log(mobilenumber);
+console.log(typeof mobilenumber);
+
+let isautomation=true 
+console.log(isautomation);
+console.log(typeof isautomation);
+
+let hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+
+
+
+
+
+
---
+++ Week1_Day2/Assignment2.js
@@ -0,0 +1,19 @@
+const browserVersion='Chrome'
+
+function getBrowserVersion(){
+    if (true) {
+        var browserVersion='Edge'      
+                   }
+    console.log(browserVersion);
+    }
+    getBrowserVersion1()
+
+
+function getBrowserVersion2() {
+    if (true){
+        let browserVersion='Edge'
+        
+    }
+    console.log(browserVersion);
+}
+getBrowserVersion2()
